3f71366e2717c2f80067aaf347d9bd669a5d5257
C.S. Jiang <christine.s.jiang@gmail.com>
2017-05-20 14:43:22 -0400

    checkperms

--- ctci/strings-arrays.js
+++ ctci/strings-arrays.js
@@ -9,3 +9,29 @@ function isUnique(str) { //assumes ASCII str input; needs greater memory if Unic
 	}
 	return true;
 }
+
+// given two strings, write a method to decide if one is a permutation of the other.
+
+function checkPerms(s1, s2) { //case-sensitive; assume whitespace is significant; only ASCII inputs
+	var l1 = s1.length;
+	var l2 = s2.length;
+	if (l1 !== l2) return false;
+
+	var chars = {};
+	for (var i = 0, l = l1; i < l; i++) {
+		if (chars[s1[i]]) chars[s1[i]] += 1;
+		else chars[s1[i]] = 1;
+	}
+	for (var i = 0, l = l2; i < l; i++) {
+		if (chars[s2[i]]) chars[s2[i]] -= 1;
+		else return false;
+
+		if (chars[s2[i]] < 0) return false;
+	}
+	return true;
+}
+
+function checkPermsSorted(s1, s2) { //same constraints, but this solution compares sorted characters of both strings
+	return s1.split('').sort().join('') === s2.split('').sort().join('');
+}
+
--- test/strings-arrays.spec.js
+++ test/strings-arrays.spec.js
@@ -12,3 +12,39 @@ describe('isUnique', function() {
 		expect(isUnique('bghfijlkANTb')).toBe(false);
 	});
 });
+
+describe('checkPerms', function() {
+	it('returns false if the strings entered are different lengths', function() {
+		expect(checkPerms('asdoij', 'ajsodiasjodiasjdoij')).toBe(false);
+		expect(checkPerms('oi34jir', 'a')).toBe(false);
+	});
+
+	it('returns true if the strings entered are empty', function() {
+		expect(checkPerms('', '')).toBe(true);
+	});
+
+	it('correctly identifies if the strings passed in are permutations of each other', function() {
+		expect(checkPerms('abcde', 'edcba')).toBe(true);
+		expect(checkPerms('dog', 'god')).toBe(true);
+		expect(checkPerms('dog', 'God')).toBe(false);
+		expect(checkPerms('dog', 'god      ')).toBe(false);
+	});
+});
+
+describe('checkPermsSorted', function() {
+	it('returns false if the strings entered are different lengths', function() {
+		expect(checkPermsSorted('asdoij', 'ajsodiasjodiasjdoij')).toBe(false);
+		expect(checkPermsSorted('oi34jir', 'a')).toBe(false);
+	});
+
+	it('returns true if the strings entered are empty', function() {
+		expect(checkPermsSorted('', '')).toBe(true);
+	});
+
+	it('correctly identifies if the strings passed in are permutations of each other', function() {
+		expect(checkPermsSorted('abcde', 'edcba')).toBe(true);
+		expect(checkPermsSorted('dog', 'god')).toBe(true);
+		expect(checkPermsSorted('dog', 'God')).toBe(false);
+		expect(checkPermsSorted('dog', 'god      ')).toBe(false);
+	});
+});
